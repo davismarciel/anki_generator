b0dfd9a30344258294838f69296b0e9bc82cff60
davismarciel <davismarciel>
2026-06-21 18:46:16 -0300

    feat: atualiza modelo de cartão para Anki com campos Coreano e Tradução

--- generator.py
+++ generator.py
@@ -6,16 +6,36 @@ model = genanki.Model(
     1607392319,
     'Generic Simple Model',
     fields=[
-        {'name': 'Termo'},
-        {'name': 'Tradução'},
+        {'name': 'Coreano'},
+        {'name': 'Traducao'},
     ],
     templates=[
         {
             'name': 'Card 1',
-            'qfmt': '{{Termo}}',
-            'afmt': '{{FrontSide}}<hr id="answer">{{Tradução}}',
+            'qfmt': '{{Coreano}}',
+            'afmt': '{{FrontSide}}<hr style="text-align: center;"id="answer">{{Traducao}}',
         },
-    ])
+    ],
+    css='''.card {
+  font-family: "Malgun Gothic", "Apple SD Gothic Neo", sans-serif;
+  font-size: 45px;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #fdfdfd;
+}
+
+.hangul {
+  font-size: 48px;
+  font-weight: bold;
+  color: #e74c3c;
+}
+
+.exemplo {
+  font-style: italic;
+  font-size: 18px;
+  color: #7f8c8d;
+  margin-top: 10px;
+}''')
 
 deck = genanki.Deck(
     2059400110,
